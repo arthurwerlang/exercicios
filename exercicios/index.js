dc8dbde0fa2748d8d26e80d5869f2ad64038f3ce
ARTHUR FRANCISCO WERLANG LIMA <03542846066@senacrs.edu.br>
2024-03-05 11:37:28 -0300

    exercicios

---
+++ exercicios/index.js
@@ -0,0 +1,26 @@
+//comando exibir mensagem
+console.log("hello word")
+
+//exercicios de calculos
+
+const num1 = 100;
+const num2 = 10;
+
+//soma
+let soma = num1 + num2;
+
+console.log("o valor da soma é " + soma + "  /está correto");
+
+
+//subtração 
+let subtração = num1 - num2;
+console.log("o valor da subtração é " + subtração + " /está correto");
+
+//multiplicação
+let multiplicação = num1 * num2;
+console.log("o valor da multiplicação é " + multiplicação + " /está correto");
+
+//divisão
+let divisão = num1 / num2;
+console.log("o valor da divisão é " + divisão + " /está correto")
+
