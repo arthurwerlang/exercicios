af1432c2a65517d9a8bece62e3fb64a73d71343c
ARTHUR FRANCISCO WERLANG LIMA <03542846066@senacrs.edu.br>
2024-03-08 11:29:27 -0300

    exercicios de votação

---
+++ 3AM/voto.js
@@ -0,0 +1,9 @@
+const idade = 9;
+
+if(idade >=18) {
+    console.log("voto obrigatorio")
+} else if (idade>=16) {
+    console.log("voto facultativo")
+}else if (idade<16) {
+    console.log("nao pode votar")
+}
